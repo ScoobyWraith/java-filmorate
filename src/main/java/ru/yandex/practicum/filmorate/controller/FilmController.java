3fb241dce8dc895691efc75c403e937da0b2442e
m.kiselev <sanwiraight@gmail.com>
2024-11-05 21:33:24 +0300

    [Sprint_10] Implement FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,49 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Long, Film> films;
+
+    FilmController() {
+        films = new HashMap<>();
+
+        films.put(1L, new Film(1L, "", "", Instant.now(), Duration.ofHours(1)));
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @GetMapping
+    public Collection<Film> getAll() {
+        return films.values();
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.Duration;
 import java.time.Instant;
 
 @Data
+@AllArgsConstructor
 public class Film {
     private Long id;
     private String name;
