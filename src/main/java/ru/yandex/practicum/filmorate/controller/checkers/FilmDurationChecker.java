a10144da6848023359a124e8593aeec8b699a384
m.kiselev <sanwiraight@gmail.com>
2024-11-06 15:51:48 +0300

    [Sprint_10] Duration -> int

--- src/main/java/ru/yandex/practicum/filmorate/controller/checkers/FilmDurationChecker.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/checkers/FilmDurationChecker.java
@@ -3,14 +3,12 @@ package ru.yandex.practicum.filmorate.controller.checkers;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
-
 public class FilmDurationChecker implements IChecker<Film> {
     @Override
     public void check(Film model) throws ValidationException {
-        final Duration duration = model.getDuration();
+        final int duration = model.getDuration();
 
-        if (duration != null && duration.isNegative()) {
+        if (duration < 0) {
             throw new ValidationException("Film's duration is negative");
         }
     }
